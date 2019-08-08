07d1137b97184b77f2bf34b83fd9f7fa157319f3
mho85 <mho85@icloud.com>
2019-08-08 12:19:18 +0200

    turnLeft and turnRight include direction change

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,15 +1,49 @@
 // Rover Object Goes Here
 // ======================
 var rover = {
-  direction: null
+  direction: "N"
 }
 
 // ======================
 function turnLeft(rover){
+  switch(rover["direction"]) {
+    case "N":
+      rover["direction"] = "W";
+      break;
+    case "S":
+      rover["direction"] = "E";
+      break;
+    case "W":
+      rover["direction"] = "S";
+      break;
+    case "E":
+      rover["direction"] = "N";
+      break;
+    default:
+      console.log("The direction of the rover is undefined.")
+      break;
+  }
   console.log("turnLeft was called!");
 }
 
 function turnRight(rover){
+  switch(rover["direction"]) {
+    case "N":
+      rover["direction"] = "E";
+      break;
+    case "S":
+      rover["direction"] = "W";
+      break;
+    case "W":
+      rover["direction"] = "N";
+      break;
+    case "E":
+      rover["direction"] = "S";
+      break;
+    default:
+      console.log("The direction of the rover is undefined.")
+      break;
+  }
   console.log("turnRight was called!");
 }
 
