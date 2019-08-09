20cbec3c40c7fc31e23be5ce488b015caed24981
mho85 <mho85@icloud.com>
2019-08-09 13:56:28 +0200

    Add props x, y. Implement moveForward().

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,7 +1,9 @@
 // Rover Object Goes Here
 // ======================
 var rover = {
-  direction: "N"
+  direction: "N",
+  x: 0,
+  y: 0
 }
 
 // ======================
@@ -41,12 +43,34 @@ function turnRight(rover){
       rover["direction"] = "S";
       break;
     default:
-      console.log("The direction of the rover is undefined.")
+      console.log("The direction of the rover is undefined.");
       break;
   }
   console.log("turnRight was called!");
 }
 
 function moveForward(rover){
-  console.log("moveForward was called")
+  switch(rover["direction"]) {
+    case "N":
+      if (rover.y !== 0) {  // Otherwise the rover stays at the same position.
+        rover.y -= 1;
+      }
+      break;
+    case "S":
+      rover.y += 1;
+      break;
+    case "W":
+      if (rover.x !== 0) {  // Otherwise the rover stays at the same position.
+        rover.x -= 1;
+      }
+      break;
+    case "E":
+      rover.x += 1;
+      break;
+    default:
+      console.log("The direction of the rover is undefined.");
+      break;
+  }
+  console.log("moveForward was called");
+  console.log("Rover's position: (" + rover.x + ", " + rover.y + ")");
 }
